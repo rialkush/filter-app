0302981b20e83ee4383e234ff086f1ebad2b7e77
rialkush <rialkush2020@gmail.com>
2022-06-13 16:05:39 +0530

    2 files modified, 1 added

--- src/components/Home/FilterPanel/index.jsx
+++ src/components/Home/FilterPanel/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import FilterListToggle from '../../common/FilterListToggle';
+import {categoryList} from '../../../constants';
 
 const FilterPanel = () => {
   return (
     <div>
       {/* <h1>Filter Panel</h1> */}
+      {/* Category */}
       <div className = 'input-group'>
         <p className="label">Category</p>
+        <FilterListToggle options = {categoryList} value = {selectedCategory} selectToggle = {selectToggle} />
       </div>
+      {/* Cuisines */}
+      {/* Price Range */}
+      {/* Star Rating */}
     </div>
   );
 };
 
 export default FilterPanel;
+ 
